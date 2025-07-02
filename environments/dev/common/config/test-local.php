c69fa001db2e3d4b6dccaa0f90c5147f027bfe3d
Anton Baranov <helpmedalph@gmail.com>
2025-07-02 14:45:24 +0500

    Наладка запуска тестов через GitHub Actions

--- environments/dev/common/config/test-local.php
+++ environments/dev/common/config/test-local.php
@@ -11,5 +11,8 @@ return [
             'password' => 'password',
             'charset' => 'utf8',
         ],
+        'request' => [
+            'cookieValidationKey' => 'test_secret_key',
+        ],
     ],
 ];
