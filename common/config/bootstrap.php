59d51eb5212b387f3e589292c017a942df4e20de
Anton Baranov <helpmedalph@gmail.com>
2025-07-06 13:56:15 +0500

    Добавлен импорт .env явно

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -1,4 +1,13 @@
 <?php
+
+use Dotenv\Dotenv;
+
+// Загружаем переменные окружения из .env файла
+if (file_exists(dirname(dirname(__DIR__)) . '/.env')) {
+    $dotenv = Dotenv::createImmutable(dirname(dirname(__DIR__)));
+    $dotenv->load();
+}
+
 Yii::setAlias('@common', dirname(__DIR__));
 Yii::setAlias('@frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('@backend', dirname(dirname(__DIR__)) . '/backend');
